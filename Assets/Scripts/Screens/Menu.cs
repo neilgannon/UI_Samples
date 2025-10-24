c456b3c1f11be94ff84e2fda32e181bc683e7ad6
Neil Gannon <neilgannon@users.noreply.github.com>
2025-10-24 12:55:59 +0100

    Added Menu Animation

--- Assets/Scripts/Screens/Menu.cs
+++ Assets/Scripts/Screens/Menu.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class Menu : ScreenBase
 {
+    Animator menuAnimator;
+
+    private void Awake()
+    {
+        menuAnimator = GetComponent<Animator>();
+    }
+
     private void OnEnable()
     {
-
+        menuAnimator.enabled = true;
+        menuAnimator.Play("Show", -1, 0);
     }
 
     private void OnDisable()
     {
-
+        menuAnimator.enabled = false;
     }
 }
